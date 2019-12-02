6b90bbce477d05fede63bb0976c384ba33f79448
екатерина луцевич <kati.18@mail.ru>
2019-12-03 01:36:05 +0300

    start

---
+++ blank/HW.02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.опишем типы данных
+            //2.
+            //3.
+            //4.
+            //5.
+            //6.
+            string textReaderResult = textReader.ReadToEnd();
+            byte binary = Convert.ToByte(Array0fTextResult[i],2);
+            byte[] imageBytes = new byte[array0fTextResult.Length - 1];
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
+        }
+    }
+}
