5efa9ea5dcd0a16b2c3d4f298651920fd0284ff4
екатерина луцевич <kati.18@mail.ru>
2019-12-14 00:52:52 +0300

    added task2

---
+++ blank/HW04.Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW04.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите один из символ W, S, A, D");
+            char ch;
+            ch =(char) Console.Read();
+            object obj = 12;
+            if (ch == 'w')
+            {
+                Console.WriteLine("Переместить фигуру вверх");
+            }
+            else if(ch == 's')
+            {
+                Console.WriteLine("Переместить фигуру вниз");
+            }
+            else if(ch == 'a')
+            {
+                Console.WriteLine("Переместить фигуру влево");
+            }
+            else if (ch == 'd')
+            {
+                Console.WriteLine("Переместить фигуру вправо");
+            }
+            else
+            {
+                Console.WriteLine("Перемещение не происходит");
+            }
+            Console.ReadKey();
+        }
+    }
+}
