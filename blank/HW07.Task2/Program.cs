ffec3da5a5285929322f9d487d4cbfb8a86d91de
екатерина луцевич <kati.18@mail.ru>
2019-12-30 03:39:47 +0300

    continue task2

--- blank/HW07.Task2/Program.cs
+++ blank/HW07.Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HW07.Task2
 {
@@ -34,8 +35,61 @@ namespace HW07.Task2
                 }
              }
             string text1 = string.Join(" ", array2);
-
             Console.WriteLine(text1);
+
+            int minLength = array1[0].Length;
+            int num2 = 0;
+            for(int i=0; i < array1.Length; i++)
+            {
+                if (array1[i].Length < minLength)
+                {
+                    minLength = array1[i].Length;
+                    num2 = i;
+                }
+            }
+            Console.WriteLine(num2);
+
+            string[] array3 = new string[array1.Length];
+            for(int i=0; i < array1.Length; i++)
+            {
+                array3[i] = array1[i];
+                if (i == num1)
+                {
+                    array3[i] = array1[i].Substring(maxLength);
+                    array3[i] = array1[num2];
+                    continue;
+                }
+                if (i == num2)
+                {
+                    array3[i] = array1[i].Substring(minLength);
+                    array3[i] = array1[num1];
+                }
+            }
+            string text2 = string.Join(" ", array3);
+            Console.WriteLine(text2);
+
+            int countLetter = 0;
+            for(int i = 0; i < array1.Length; i++)
+            {
+                countLetter += array1[i].Length;
+            }
+            Console.WriteLine($"Количество букв: {countLetter}");
+            int countPunctuation = text.Count(char.IsPunctuation);
+            Console.WriteLine($" Количество знаков пунктуации: {countPunctuation}");
+
+            string[] array4 = new string[array1.Length];
+            int max = array1[0].Length;
+            for(int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length > max)
+                {
+                    max = array1[i].Length;
+                    
+                }
+                Console.WriteLine(array1[i]);
+                //array1.Sort
+            }
+            
 
             Console.ReadKey();
         }
