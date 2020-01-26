87dd9f8ff9627cf98385e0a0a264e8a3c45d12a1
екатерина луцевич <kati.18@mail.ru>
2020-01-26 19:23:13 +0300

    added code

---
+++ blank/HW12.Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HW12.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MotorcycleRepository repository = new MotorcycleRepository();
+
+            repository.GetMotorcycleById(2);
+            repository.GetMotorcycles();
+            repository.CreateMotorcycle(new Motorcycle()
+            {
+                Id = 4,
+                Name = "Hors",
+                Model = "F - 160",
+                Year = 2018,
+                Odometer = 4_100,
+            });
+            repository.UpdateMotorcycle(new Motorcycle()
+            {
+                Id = 1,
+                Name = "Yamaha",
+                Model = "LK100",
+                Year = 2019,
+                Odometer = 1_300,
+            });
+            repository.DeleteMotorcycle(3);
+
+            Console.ReadLine();
+        }
+    }
+}
