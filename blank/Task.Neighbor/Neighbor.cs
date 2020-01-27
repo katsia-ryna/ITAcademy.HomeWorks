8595bcb571617f6baafb9f0c8b00870288a26a30
екатерина луцевич <kati.18@mail.ru>
2020-01-28 00:37:55 +0300

    add code

---
+++ blank/Task.Neighbor/Neighbor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Task.Neighbor
+{
+    class Neighbor
+    {
+        public string _fullName;
+        private int _flatNumber;
+        public int _phoneNumber;
+
+        private List<Neighbor> _floorNeighbors;
+        public Neighbor()
+        {
+            _floorNeighbors = new List<Neighbor>()
+            {
+                new Neighbor()
+                {
+                    _fullName="Kie",
+                    _flatNumber=44,
+                    _phoneNumber=123567
+                },
+                new Neighbor()
+                {
+                    _fullName="Tom",
+                    _flatNumber=45,
+                    _phoneNumber=2412763
+                },
+                new Neighbor()
+                {
+                    _fullName="Molly",
+                    _flatNumber=46,
+                    _phoneNumber=78956241
+                }
+            };
+        }
+
+        public Neighbor Print(int num)
+        {
+            Console.WriteLine("Enter the flat number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            /*if (_flatNumber==number)
+            {
+                //Console.WriteLine($"phone number = {_phoneNumber} /n name floor neighbor = {_fullName}");
+
+            }*/
+            return _floorNeighbors.FirstOrDefault(t => t._flatNumber == number);
+        }
+
+    }
+}
---
+++ blank/Task.Neighbor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task.Neighbor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Neighbor neighbor = new Neighbor();
+            //neighbor.Print();
+            Console.WriteLine("Enter the flat number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            var num1=neighbor.Print(number);
+            PrintNeighbor(num1);
+
+            Console.ReadLine();
+        }
+        static void PrintNeighbor(Neighbor neighbor)
+        {
+            Console.WriteLine($"{neighbor._phoneNumber}, {neighbor._fullName}");
+        }
+    }
+}
